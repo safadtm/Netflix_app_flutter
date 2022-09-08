cf5eeccc1e7e5a228d6976049421eb9d35b0aa3a
safadtm <safadtm2004@gmail.com>
2022-09-08 20:47:24 +0530

    Download page on progress 2

--- lib/presentation/downloads/screen_downloads.dart
+++ lib/presentation/downloads/screen_downloads.dart
@@ -28,60 +28,117 @@ class ScreenDownloads extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Row(children: const [
-            kWidth,
-            Icon(
-              Icons.settings,
-              color: kWhiteColor,
-            ),
-            kWidth,
-            Text('Smart Downloads'),
-          ]),
-          const Text('Introducing Downloads for you'),
+          _SmartDownloads(),
           const Text(
-              'We will download a personlised selection of movies and shows for you so there is always something to watch on your device'),
-          Container(
+            'Introducing Downloads for you',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: kWhiteColor,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          kHeight,
+          const Text(
+            'We will download a personlised selection of\n movies and shows for you so there is always\n something to watch on your\n device',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+            ),
+          ),
+          kHeight,
+          SizedBox(
             width: size.width,
             height: size.height,
-            color: Colors.white,
             child: Stack(
               alignment: Alignment.center,
               children: [
                 Center(
                   child: CircleAvatar(
                     radius: size.width * 0.4,
+                    backgroundColor: Colors.grey.withOpacity(0.5),
                   ),
                 ),
-                DownloadsImageWidget(imageList: imageList)
+                DownloadsImageWidget(
+                  imageList: imageList[0],
+                  margin: const EdgeInsets.only(
+                    left: 130,
+                    bottom: 50,
+                  ),
+                  angle: 20,
+                  size: Size(size.width * 0.4, size.width * 0.58),
+                ),
+                DownloadsImageWidget(
+                  imageList: imageList[1],
+                  margin: const EdgeInsets.only(
+                    right: 130,
+                    bottom: 50,
+                  ),
+                  angle: -20,
+                  size: Size(size.width * 0.4, size.width * 0.58),
+                ),
+                DownloadsImageWidget(
+                  imageList: imageList[2],
+                  radius: 8,
+                  margin: const EdgeInsets.only(bottom: 10),
+                  size: Size(size.width * 0.45, size.width * 0.65),
+                ),
               ],
             ),
           ),
           MaterialButton(
             color: kButtonColorBlue,
             onPressed: () {},
-            child: const Text(
-              'Set up',
-              style: TextStyle(
-                color: kWhiteColor,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: const Text(
+                'Set up',
+                style: TextStyle(
+                  color: kWhiteColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
           MaterialButton(
             color: kButtonColorWhite,
             onPressed: () {},
-            child: const Text(
-              'See what you can download',
-              style: TextStyle(
-                color: kBlackColor,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: const Text(
+                'See what you can download',
+                style: TextStyle(
+                  color: kBlackColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Row _SmartDownloads() {
+    return Row(
+      children: const [
+        kWidth,
+        Icon(
+          Icons.settings,
+          color: kWhiteColor,
+        ),
+        kWidth,
+        Text('Smart Downloads'),
+      ],
     );
   }
 }
@@ -90,24 +147,35 @@ class DownloadsImageWidget extends StatelessWidget {
   const DownloadsImageWidget({
     Key? key,
     required this.imageList,
+    this.angle = 0,
+    required this.margin,
+    required this.size,
+    this.radius = 10,
   }) : super(key: key);
 
-  final List imageList;
+  final String imageList;
+  final double angle;
+  final EdgeInsets margin;
+  final Size size;
+  final double radius;
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-
     return Transform.rotate(
-      angle: 20 * pi / 180,
-      child: Container(
-        width: size.width * 0.4,
-        height: size.width * 0.58,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-            image: NetworkImage(
-              imageList[0],
+      angle: angle * pi / 180,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          margin: margin,
+          width: size.width,
+          height: size.height,
+          decoration: BoxDecoration(
+            //  color: Colors.white30,
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                imageList,
+              ),
             ),
           ),
         ),

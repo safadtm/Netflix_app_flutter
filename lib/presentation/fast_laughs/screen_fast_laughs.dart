4b1cb44ead2c70f8db21d31b7d122fe5e9e82d42
safadtm <safadtm2004@gmail.com>
2022-09-09 15:06:17 +0530

    Screen Fast & Laughs UI with dummy data done

--- lib/presentation/fast_laughs/screen_fast_laughs.dart
+++ lib/presentation/fast_laughs/screen_fast_laughs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_app/presentation/fast_laughs/widgets/video_list_item.dart';
 
 class ScreenFastLaughs extends StatelessWidget {
   const ScreenFastLaughs({super.key});
@@ -6,7 +7,19 @@ class ScreenFastLaughs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('ScreenFastLaughs'),
+      body: SafeArea(
+        child: PageView(
+          scrollDirection: Axis.vertical,
+          children: List.generate(
+            10,
+            (index) {
+              return VideoListItem(
+                index: index,
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
